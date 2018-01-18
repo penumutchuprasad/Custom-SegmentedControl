5677fa386613595d7b96c25cb7822be30918e5fa
Leela Prasad <leelaprasad@leelas-mac-mini.local>
2018-01-18 14:08:58 +0530

    Initial Commit

---
+++ CustomSEgmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSEgmentedControl
+//
+//  Created by Leela Prasad on 18/01/18.
+//  Copyright © 2018 Leela Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
